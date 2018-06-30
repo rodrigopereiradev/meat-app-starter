db5f602c62034fc431ac05aa98235a9fae72d1ad
Rodrigo Pereira <rodrigopereira.dev@gmai.com>
2018-06-30 18:43:03 -0300

    add tratamento de erros

--- src/app/restaurants/restaurants.service.ts
+++ src/app/restaurants/restaurants.service.ts
@@ -2,8 +2,10 @@ import { Injectable } from '@angular/core';
 import { Http } from '@angular/http'
 import { Restaurant } from './restaurant/restaurant.model'
 import { MEAT_API } from '../app.api'
+import { ErrorHandler } from '../app.error-handler'
 import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/operator/map'
+import 'rxjs/add/operator/catch'
 
 @Injectable()
 export class RestaurantsService {
@@ -11,7 +13,8 @@ export class RestaurantsService {
   constructor(private http: Http) {}
 
   restaurants(): Observable<Restaurant[]> {
-    return this.http.get(`${MEAT_API}/restaurants`)
+    return this.http.get(`${MEAT_API}/restaurants1`)
       .map(response =>response.json())
+      .catch(ErrorHandler.handleError)
   }
 }
